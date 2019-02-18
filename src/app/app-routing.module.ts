f0299a57d63793c58ea52684fd277d23f4870eda
javier muñoz <x_jemd_x@hotmail.com>
2019-02-17 22:58:01 -0300

    iniciando proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component'
+import { ContactComponent } from './pages/contact/contact.component'
+import { RepositoriesComponent } from './pages/repositories/repositories.component'
 
-const routes: Routes = [];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+export const AppRoutes: Routes = [
+  { path: 'home', component: HomeComponent},
+  { path: 'contact', component: ContactComponent},
+  { path: 'repositories', component: RepositoriesComponent},
+  { path: '**', redirectTo: '/home' }
+]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
+import { MaterializeModule } from 'angular2-materialize'
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router'
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutes } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { RepositoriesComponent } from './pages/repositories/repositories.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HabilitiesComponent } from './pages/habilities/habilities.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactComponent,
+    RepositoriesComponent,
+    NavbarComponent,
+    FooterComponent,
+    HabilitiesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    //MaterializeModule,
+    RouterModule.forRoot(AppRoutes, {useHash: true})
   ],
   providers: [],
   bootstrap: [AppComponent]
